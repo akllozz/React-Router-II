75a4b8617c1e3c5163cdb88cbc0324052285587a
Anakaren Lozano <aklozzg@gmail.com>
2024-06-16 19:04:46 -0400

    update deployment

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/pokemons" element={<Pokemons />} />
         <Route path="/pokemons/:name" element={<Details />} />
       </Routes>
